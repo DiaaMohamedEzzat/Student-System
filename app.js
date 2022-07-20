c39b7bae0bed260cf2b98604ef4013bfe65b10f6
DiaaMohamedEzzat <108733228+DiaaMohamedEzzat@users.noreply.github.com>
2022-07-20 15:45:51 +0200

    new

---
+++ app.js
@@ -0,0 +1,69 @@
+const yargs = require("yargs");
+//add
+const details = require("./details");
+yargs.command({
+  command: "add",
+  describe: "Add data of students",
+  builder: {
+    id: {
+      describe: "This is id of student",
+      demandOption: true,
+      type: "number",
+    },
+    name: {
+      describe: "This is name of student",
+      demandOption: true,
+      type: "string",
+    },
+    grads: {
+      describe: "This is grads of student",
+      demandOption: true,
+      type: "array",
+    },
+  },
+  handler: (x) => {
+    details.addStudent(x.id, x.name, x.grads);
+  },
+});
+
+delete yargs.command({
+  command: "delete",
+  describe: "Delete student",
+  builder: {
+    id: {
+      describe: "This is id description in delete command",
+      demandOption: true,
+      type: "number",
+    },
+  },
+  handler: (x) => {
+    details.deleteStudent(x.id);
+  },
+});
+
+//read
+yargs.command({
+  command: "read",
+  describe: "Read student",
+  builder: {
+    id: {
+      describe: "This is id in read command",
+      demandOption: true,
+      type: "number",
+    },
+  },
+  handler: (x) => {
+    details.readStudents(x.id);
+  },
+});
+
+// listAll
+yargs.command({
+  command: "list",
+  describe: "List students",
+  handler: () => {
+    details.listStudents();
+  },
+});
+
+yargs.parse();
---
+++ details.js
@@ -0,0 +1,96 @@
+const fs = require("fs");
+//add
+
+const addStudent = (id, name, grads) => {
+  const students = loadStudents();
+
+  const duplicateTitle = students.find((student) => {
+    return student.id === id;
+  });
+  console.log(duplicateTitle);
+ 
+  if (!duplicateTitle) {
+    total = 0;
+    grads.forEach((grade) => {
+      total += parseInt(grade);
+    });
+    students.push({
+      id,
+      name,
+      grads,
+      total,
+    });
+
+    saveStudents(students);
+    console.log("student is saved successfuly");
+  } else {
+    console.log("Error ,this student is already saved");
+  }
+};
+const loadStudents = () => {
+  try {
+    const dataBuffer = fs.readFileSync("students.json").toString();
+
+    return JSON.parse(dataBuffer);
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveStudents = (students) => {
+  const saveData = JSON.stringify(students);
+  fs.writeFileSync("students.json", saveData);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+// delete
+const deleteStudent = (id) => {
+  const students = loadStudents();
+  const studentsToKeep = students.filter((student) => {
+    return student.id !== id;
+  });
+
+  if (students.length !== studentsToKeep.length) {
+    saveStudents(studentsToKeep);
+    console.log("this student is removed");
+  } else {
+    console.log("this id is found.. Please try again");
+  }
+};
+
+// /////////////////////////////////////////////////////////////////////////////////
+
+// read
+
+const readStudents = (id) => {
+  const students = loadStudents();
+  const student = students.find((student) => {
+    return student.id === id;
+  });
+
+  if (student) {
+    console.log(student);
+  } else {
+    console.log("this id is not found");
+  }
+};
+
+//list
+const listStudents = () => {
+  const students = loadStudents();
+  students.forEach((student) => {
+    console.log(
+      "Student id is " + student.id,
+      " , Student Name is " + student.name,
+      " , Student grads " + student.grads + "  total is " + student.total
+    );
+  });
+};
+
+module.exports = {
+  addStudent,
+  deleteStudent,
+  readStudents,
+  listStudents,
+};
